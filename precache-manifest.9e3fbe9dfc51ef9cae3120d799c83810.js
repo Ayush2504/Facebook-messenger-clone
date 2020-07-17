6e6c3db3c5747cc8942b65b77d8d2900a32b1bc5
AYUSH SONI <ayushakshay.soni@gmail.com>
2020-07-17 06:58:05 +0000

    Updates

---
+++ precache-manifest.9e3fbe9dfc51ef9cae3120d799c83810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f5278615f5e462d875a5a021cac3d6",
+    "url": "/Facebook-messenger-clone/index.html"
+  },
+  {
+    "revision": "1cb76bca34c2b809c842",
+    "url": "/Facebook-messenger-clone/static/css/main.b23106f3.chunk.css"
+  },
+  {
+    "revision": "a70d77d410b9f78c1780",
+    "url": "/Facebook-messenger-clone/static/js/2.8f92c016.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Facebook-messenger-clone/static/js/2.8f92c016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb76bca34c2b809c842",
+    "url": "/Facebook-messenger-clone/static/js/main.dd4b39ef.chunk.js"
+  },
+  {
+    "revision": "7255b1df59c93257af83",
+    "url": "/Facebook-messenger-clone/static/js/runtime-main.29ab244e.js"
+  }
+]);
